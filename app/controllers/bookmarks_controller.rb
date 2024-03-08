0646b5f370ca77adcabb536a806a5599ce66e1a5
Joshua Koch <dreimalr@gmail.com>
2024-03-08 16:06:35 +0100

    added_bookmark_remove

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,6 +15,11 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
   private
 
   def bookmarks_params
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resources :bookmarks, only: [:new, :create]
   end
 
+  resources :bookmarks, only: :destroy
 
 
 end
